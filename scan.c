44ec63a4d40edcdd4d294b9b422fc054f27acbe1
shuneish <grothendieck57@icloud.com>
2024-10-19 11:25:37 +0900

    Ask GPT(Name とprogramだけおかしいけど)

--- scan.c
+++ scan.c
@@ -19,181 +19,151 @@ int scan(){
     char buf[MAXSTRSIZE];
     int i = 0;
 
-        cbuf = fgetc(file);
+    cbuf = fgetc(file);
 
-        if (cbuf == EOF) return S_ERROR;
+    if (cbuf == EOF) return S_ERROR;
 
-        switch (cbuf){
-            /* separator */
-            case ' ':
-            case '\t':
-                break;
-            case '\n':
-            case '\r':
-                check_lineBreak();
-                break;
-            /* comment */
-            case '{':
-                while (cbuf != EOF){
-                    cbuf = fgetc(file);
-                    if (cbuf == '}') break;
+    switch (cbuf) {
+        /* separator */
+        case ' ':
+        case '\t':
+            break;
+        case '\n':
+        case '\r':
+            check_lineBreak();
+            break;
+        /* comment */
+        case '{':
+            while ((cbuf = fgetc(file)) != EOF) {
+                if (cbuf == '}') break;
+                if (cbuf == '\n' || cbuf == '\r') check_lineBreak();
+            }
+            if (cbuf == EOF) return error("Comment is not closed");
+            break;
+        case '/':
+            cbuf = fgetc(file);
+            if (cbuf == '*') {
+                while ((cbuf = fgetc(file)) != EOF) {
+                    if (cbuf == '*') {
+                        cbuf = fgetc(file);
+                        if (cbuf == '/') break;
+                    }
                     if (cbuf == '\n' || cbuf == '\r') check_lineBreak();
                 }
-                return error("Comment is not closed");
-            case '/':
-                cbuf = fgetc(file);
-                if (cbuf == '*'){
-                    while (cbuf != EOF){
-                        cbuf = fgetc(file);
-                        if (cbuf == '*'){
-                            cbuf = fgetc(file);
-                            if (cbuf == '/') break;
-                        }
-                        if (cbuf == '\n' || cbuf == '\r') check_lineBreak();
-                    }
-                    break;
-                } else {
-                    ungetc(cbuf, file);
-                    return TDIV;
-                }
-                break;
-
-            /* symbol */
-            case '+':
-                return TPLUS;
-            case '-':
-                return TMINUS;
-            case '*':
-                return TSTAR;
-            case '(':
-                return TLPAREN;
-            case ')':
-                return TRPAREN;
-            case '[':
-                return TLSQPAREN;
-            case ']':
-                return TRSQPAREN;
-            case '.':
-                return TDOT;
-            case ',':
-                return TCOMMA;
-            case ':':
-                cbuf = fgetc(file);
-                if (cbuf == '=') return TASSIGN;
+                if (cbuf == EOF) return error("Comment is not closed");
+            } else {
                 ungetc(cbuf, file);
-                return TCOLON;
-            case ';':
-                return TSEMI;
-            case '=':  
-                return TEQUAL;
-            case '<':
+                return TDIV;
+            }
+            break;
+
+        /* symbol */
+        case '+': return TPLUS;
+        case '-': return TMINUS;
+        case '*': return TSTAR;
+        case '(': return TLPAREN;
+        case ')': return TRPAREN;
+        case '[': return TLSQPAREN;
+        case ']': return TRSQPAREN;
+        case '.': return TDOT;
+        case ',': return TCOMMA;
+        case ':':
+            cbuf = fgetc(file);
+            if (cbuf == '=') return TASSIGN;
+            ungetc(cbuf, file);
+            return TCOLON;
+        case ';': return TSEMI;
+        case '=': return TEQUAL;
+        case '<':
+            cbuf = fgetc(file);
+            if (cbuf == '>') return TNOTEQ;
+            if (cbuf == '=') return TLEEQ;
+            ungetc(cbuf, file);
+            return TLE;
+        case '>':
+            cbuf = fgetc(file);
+            if (cbuf == '=') return TGREQ;
+            ungetc(cbuf, file);
+            return TGR;
+
+        /* string */
+        case '\'':
+            i = 0;
+            while (1) {
                 cbuf = fgetc(file);
-                if (cbuf == '>') return TNOTEQ;
-                if (cbuf == '=') return TLEEQ;
-                ungetc(cbuf, file);
-
-                return TLE;
-            case '>':
-                cbuf = fgetc(file);
-                if (cbuf == '=') return TGREQ;
-                ungetc(cbuf, file);
-
-                return TGR;
-            /* string */
-            case '\'' :
-                i = 0;
-
-                while (1){
-                    if (cbuf == EOF || cbuf == '\n' || cbuf == '\r') return error("String is not closed");
-                
+                if (cbuf == EOF || cbuf == '\n' || cbuf == '\r') return error("String is not closed");
+                if (cbuf == '\'') {
                     cbuf = fgetc(file);
-                    buf[i++] = cbuf;
-                    if (cbuf == '\''){
-                        cbuf = fgetc(file);
-                        if (cbuf != '\'') {
-                            buf[i] = '\0';
-                            strcpy(string_attr, buf);
-                            break;
-                        }
+                    if (cbuf != '\'') {
+                        buf[i] = '\0';
+                        strcpy(string_attr, buf);
                         ungetc(cbuf, file);
+                        break;
                     }
                 }
-
-                buf[i] = '\0';
-                strcpy(string_attr, buf);
-
-                return TSTRING;
-            /* unsigned integer */
-            case '0': case '1': case '2': case '3': case '4':
-            case '5': case '6': case '7': case '8': case '9':
-                i = 0;
-                num_attr = 0;
                 buf[i++] = cbuf;
-                while (isdigit(cbuf = fgetc(file))){
-                    buf[i++] = cbuf;
-                    num_attr = 10 * num_attr + (cbuf - '0');
+                if (i >= MAXSTRSIZE - 1) return error("String too long");
+            }
+            return TSTRING;
 
-                    if(num_attr > MAXINT){
-                        return error("Number is too large");
-                    }
+        /* unsigned integer */
+        case '0': case '1': case '2': case '3': case '4':
+        case '5': case '6': case '7': case '8': case '9':
+            i = 0;
+            num_attr = 0;
+            buf[i++] = cbuf;
+            while (isdigit(cbuf = fgetc(file))) {
+                buf[i++] = cbuf;
+                num_attr = 10 * num_attr + (cbuf - '0');
+                if (num_attr > MAXINT) return error("Number is too large");
+            }
+            buf[i] = '\0';
+            strcpy(string_attr, buf);
+            ungetc(cbuf, file);
+            return TNUMBER;
+
+        /* keyword or name */
+        default:
+            if (isalpha(cbuf)) {
+                i = 0;
+                buf[i++] = cbuf;
+                while (isalnum(cbuf = fgetc(file)) || isalpha(cbuf)) {
+                    buf[i++] = cbuf;
                 }
                 buf[i] = '\0';
                 strcpy(string_attr, buf);
                 ungetc(cbuf, file);
-                return TNUMBER;
-            /* keyword or name */
-            default:
-                if (isalpha(cbuf)){
-                    char buf[MAXSTRSIZE];
-                    int i = 0;
-                    buf[i++] = cbuf;
-                    while (isalnum(cbuf = fgetc(file)) || isalpha(cbuf)){
-                        buf[i++] = cbuf;
+
+                for (i = 0; i < KEYWORDSIZE; i++) {
+                    if (strcmp(buf, key[i].keyword) == 0) {
+                        return key[i].keytoken;
                     }
-                    buf[i] = '\0';
-                    strcpy(string_attr, buf);
-                    ungetc(cbuf, file);
-
-                    for (i = 0; i < KEYWORDSIZE; i++){
-                        if (strcmp(buf, key[i].keyword) == 0){
-                            return key[i].keytoken;
-                        }
-                    }
-
-                    return TNAME;
-
-                } else {
-                    return error("Illegal character");
                 }
-        }
+                return TNAME;
+            } else {
+                return error("Illegal character");
+            }
+    }
 
     return 0;
 }
 
-int get_linenum(){
+int get_linenum() {
     return linenum;
 }
 
-void end_scan(){
+void end_scan() {
     fclose(file);
 }
 
-void check_lineBreak(){
-    if (cbuf == '\n'){
-       cbuf = fgetc(file);
-                if (cbuf == '\r'){
-                    /* \n\r */
-                } else {
-                    ungetc(cbuf, file);
-                }
-                linenum++;
-    } else if (cbuf == '\r'){
+void check_lineBreak() {
+    if (cbuf == '\n') {
         cbuf = fgetc(file);
-                if (cbuf == '\n'){
-                    /* \r\n */
-                } else {
-                    ungetc(cbuf, file);
-                }
-                linenum++;
+        if (cbuf != '\r') ungetc(cbuf, file);
+        linenum++;
+    } else if (cbuf == '\r') {
+        cbuf = fgetc(file);
+        if (cbuf != '\n') ungetc(cbuf, file);
+        linenum++;
     }
 }
